3789b058d81e07eac019aed43ef817abb594b623
Ægir Örn Símonarson <agirorn@gmail.com>
2025-08-24 23:56:26 +0000

    Refactoring and Documenting

--- eventsourced_core/src/lib.rs
+++ eventsourced_core/src/lib.rs
@@ -16,6 +16,32 @@ pub trait Aggregate: Sized {
     type State;
     type AggregateId: Send + 'static;
 
+    /// Implement this function to handle any command that should be executed on the Aggregate.
+    async fn handle(&mut self, cmd: Self::Command) -> Result<Self::CommandResult, Self::Error>;
+
+    /// Applies each event on to the aggregate.
+    ///
+    /// This function is only applying facts on to the aggregate and should need other systems to
+    /// get to the connect state and should should not call out to other systems to get any
+    /// information. All the facts should be in the events that are applied.
+    fn apply(&mut self, event: Self::Event, save: bool) -> Result<(), Self::Error>;
+
+    /// Implement this function to return any event that needs to be persisted after a successfully
+    /// handling of a command.
+    fn get_uncommitted_events(&mut self) -> Vec<Self::Event>;
+
+    /// Return the current state of the Aggregate.
+    fn get_state(&self) -> &Self::State;
+
+    /// Create a new fresh Aggregate by aggregate_id
+    fn new(aggregate_id: &Self::AggregateId) -> Self;
+
+    /// Full execution of a command with persistence.
+    ///
+    /// The default implementation loads the Aggregate into memory and then streams in all the
+    /// saved events for the aggregate and applies them one after the other to get the aggregate
+    /// state up to date before executing the command using the handle function and then storing
+    /// the any new events in the event store.
     async fn execute<ES>(
         event_store: &mut ES,
         cmd: Self::Command,
@@ -35,8 +61,10 @@ pub trait Aggregate: Sized {
         Ok(result)
     }
 
-    async fn handle(&mut self, cmd: Self::Command) -> Result<Self::CommandResult, Self::Error>;
-
+    /// Loads a single aggregate form the event store.
+    ///
+    /// The default implementation does this by constructing a new Aggregate and the applying all
+    /// events stored in the event store to the Aggregate
     async fn load<ES>(
         event_store: &ES,
         aggregate_id: Self::AggregateId,
@@ -53,16 +81,6 @@ pub trait Aggregate: Sized {
         }
         Ok(user)
     }
-
-    /// Applies each event on to the aggregate.
-    ///
-    /// This function is only applying facts on to the aggregate and should need other systems to
-    /// get to the connect state and should should not call out to other systmes to get any
-    /// information. All the facts should be in the events that are applied.
-    fn apply(&mut self, event: Self::Event, save: bool) -> Result<(), Self::Error>;
-    fn get_uncommitted_events(&mut self) -> Vec<Self::Event>;
-    fn get_state(&self) -> &Self::State;
-    fn new(aggregate_id: &Self::AggregateId) -> Self;
 }
 
 pub trait Command<Id> {
